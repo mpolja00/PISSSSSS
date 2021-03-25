60a522e315fb74324897159c8240d8b0d8199a33
mpolja00 <mpolja00@fesb.hr>
2021-03-25 20:56:04 +0100

    dodavanje nove klase

---
+++ PISSSSSS/NewClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PISSSSSS
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ovo je nova klasa!");
+        }
+    }
+}
